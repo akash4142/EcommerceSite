6a8c9ec399a4dc69ccd5d98912f3a7c398d6896b
akash yadav <64635004+akash4142@users.noreply.github.com>
2024-01-10 22:13:16 -0500

    initial commit

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/shop',(req,res)=>{
     res.sendFile(path.join(__dirname,'shop.html'))
 })
 
+app.get('/about',(req,res)=>{
+    res.sendFile(path.join(__dirname,'about.html'))
+})
+
 app.listen(HTTP_PORT,()=>{
     console.log('server is listening on port ' + HTTP_PORT)
 })
